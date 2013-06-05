48d995e366f4259c4d996bf7e1aeb6e4bbbc4a15
Patrick Garvey <garvey2489@hotmail.com>
2013-06-05 15:34:59 +0100

    File management
    Adding DB byte values from the instructions

---
+++ Bodgitt and Scarper/src/suncertify/db/DBFileManager.java
@@ -0,0 +1,19 @@
+package suncertify.db;
+
+public class DBFileManager {
+	
+	//bytes that store the Magic Cookie value as specified in instructions
+	private static final int MAGIC_COOKIE_VALUE = 4;
+	
+	private static final int NUMBER_OF_FIELDS = 2;
+	
+	private static final int FIELD_NAME_LENGHT = 1;
+	
+	//What do I put in for n value?
+	
+	private static final int FIELD_LENGHT = 1;
+	
+	private static final int FLAG_BYTE = 1;
+	
+	private static final int VALID = 00; 
+}
