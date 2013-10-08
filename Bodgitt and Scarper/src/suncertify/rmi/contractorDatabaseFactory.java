e414a99f3ebea0e490914de001799b97ac25f6a4
epagarv <patrick.garvey@ericsson.com>
2013-10-08 09:57:44 +0100

    RMI  start
    Adding the RMI facotry interface and the the remote interface

---
+++ Bodgitt and Scarper/src/suncertify/rmi/contractorDatabaseFactory.java
@@ -0,0 +1,15 @@
+package suncertify.rmi;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+
+public interface contractorDatabaseFactory  extends Remote{
+	
+	/**
+     * Returns a reference to a remote instance of a class unique to the
+     * connecting
+     * **/
+	 public contractorRemote getClient() throws RemoteException;
+
+}
---
+++ Bodgitt and Scarper/src/suncertify/rmi/contractorRemote.java
@@ -0,0 +1,10 @@
+package suncertify.rmi;
+
+import java.rmi.Remote;
+
+import suncertify.db.DBMain;
+
+
+public interface contractorRemote extends Remote, DBMain {
+
+}
