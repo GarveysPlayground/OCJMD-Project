35722e017d6c8b514fefa591c6f79987c0c2a6cf
Patrick Home <garvey2489@hotmail.com>
2013-10-29 22:57:03 +0000

    Computer crash

--- Bodgitt and Scarper/src/suncertify/db/Data.java
+++ Bodgitt and Scarper/src/suncertify/db/Data.java
@@ -18,8 +18,8 @@ import java.io.IOException;
  * from the functional code and are left with with some simple to understand
  * APIs that future coders can implement. 
  * 
- * It also allows us to break the code into multiple classes with a single
- * purpose i.e {@link #DataAccess()} for record manipulation and 
+ * It also allows us to break the code into multiple classes that focus on a
+ * single purpose i.e {@link #DataAccess()} for record manipulation and 
  * {@link #LockManager()} for record locking.
  */
 public class Data implements DBMain {
@@ -47,58 +47,85 @@ public class Data implements DBMain {
 	}
 	
 	
-	/* (non-Javadoc)
-	 * This method calls on the read implementation located at
+	/**
+	 * This method calls on the read implementation located at 
+	 * {@link suncertify.db.DataAccess#read(int)}.
+	 * 
+	 * @param recNo : The record number to be read
+	 * @return returns a String [] of the read record
+	 * @throws RecordNotFoundException if @param recNo is not found
 	 * @see suncertify.db.DataAccess#read(int)
 	 * @see suncertify.db.DBMain#read(int)
 	 */
 	@Override
-	public String[] read(final int recNo) throws RecordNotFoundException {
+	public final String[] read(final int recNo) throws RecordNotFoundException {
 			return DataAccess.read(recNo);
 	}
 
 	
-	/* (non-Javadoc)
-	 * This method calls on the update implementation located at
-	 * @see suncertify.db.DataAccess#update(int, java.lang.String[])
-	 * @see suncertify.db.DBMain#update(int, java.lang.String[])
+	/**
+	 * This method calls on the update(int, String []) implementation 
+	 * located at  {@link suncertify.db.DataAccess#update(int, String[])}.
+	 * 
+	 * @param recNo : The record number to be read
+	 * @param data : String [] to update the record with
+	 * @throws RecordNotFoundException if @param recNo is not found
+	 * @see suncertify.db.DataAccess#update(int, String[])
+	 * @see suncertify.db.DBMain#update(int, String[])
 	 */
 	@Override
-	public void update(final int recNo, final String[] data) 
+	public final void update(final int recNo, final String[] data) 
 			throws RecordNotFoundException {
 		DataAccess.update(recNo, data);		
 	}
 
 	
-	/* (non-Javadoc)
-	 * This method calls on the update implementation located at
+	/**
+	 * This method calls on the delete(int) implementation 
+	 * located at  {@link suncertify.db.DataAccess#delete(int)}.
+	 * 
+	 * @param recNo : The record number to be read
+	 * @throws RecordNotFoundException if @param recNo is not found
 	 * @see suncertify.db.DataAccess#delete(int)
-	 * @see suncertify.db.DBMain#delete(java.lang.String[])
+	 * @see suncertify.db.DBMain#delete(int)
 	 */
 	@Override
-	public void delete(final int recNo) throws RecordNotFoundException {
+	public final void delete(final int recNo) throws RecordNotFoundException {
 		DataAccess.delete(recNo);
 	}
 
 	
-	/* (non-Javadoc)
-	 * This method calls on the update implementation located at
-	 * @see suncertify.db.DataAccess#(java.lang.String[])
-	 * @see suncertify.db.DBMain#find(java.lang.String[])
+	/**
+	 * This method calls on the find(String []) implementation 
+	 * located at  {@link suncertify.db.DataAccess#find(String[])}.
+	 * 
+
+	 * @param criteria : String [] to update the record with
+	 * @throws RecordNotFoundException if @param recNo is not found
+	 * @see suncertify.db.DataAccess#find(String[])
+	 * @see suncertify.db.DBMain#find(String[])
+	 * @return int [] or matching record numbers
 	 */
 	@Override
-	public int[] find(final String[] criteria) throws RecordNotFoundException {
+	public final int[] find(final String[] criteria) 
+			throws RecordNotFoundException {
 		return DataAccess.find(criteria);
 	}
 
 	
-	/* (non-Javadoc)
-	 * This method calls on the update implementation located at
-	 * @see suncertify.db.DataAccess#create(java.lang.String[])
-	 * @see suncertify.db.DBMain#create(java.lang.String[])
+	/**
+	 * This method calls on the create(String []) implementation 
+	 * located at  {@link suncertify.db.DataAccess#create(String[])}.
+	 * 
+
+	 * @param data : String [] of new record values
+	 * @throws DuplicateKeyException if new record already exists
+	 * @see suncertify.db.DataAccess#create(String[])
+	 * @see suncertify.db.DBMain#create(String[])
+	 * @return int of new record number
 	 */
 	@Override
-	public int create(final String[] data) throws DuplicateKeyException {
+	public final int create(final String[] data) throws DuplicateKeyException {
 		int newRecord = 0;	
 		try {
 			newRecord = DataAccess.create(data);
@@ -108,40 +135,55 @@ public class Data implements DBMain {
 				e.printStackTrace();
 			}
 		return newRecord;
-
-
 	}
 
 	
-	/* (non-Javadoc)
-	 * This method calls on the update implementation located at
+	/**
+	 * This method calls on the lock(int) implementation 
+	 * located at  {@link suncertify.db.LockManager#lock(int)}.
+	 * 
+
+	 * @param recNo : record number to be locked
+	 * @throws RecordNotFoundException if new record does'nt exists
 	 * @see suncertify.db.LockManager#lock(int)
 	 * @see suncertify.db.DBMain#lock(int)
 	 */
 	@Override
-	public void lock(final int recNo) throws RecordNotFoundException {
+	public final void lock(final int recNo) throws RecordNotFoundException {
 		lockManager.lock(recNo);
 	}
 
 	
-	/* (non-Javadoc)
-	 * This method calls on the update implementation located at
+	/**
+	 * This method calls on the unlock(int) implementation 
+	 * located at  {@link suncertify.db.LockManager#unlock(int)}.
+	 * 
+
+	 * @param recNo : record number to be unlocked
+	 * @throws RecordNotFoundException if new record does'nt exists
 	 * @see suncertify.db.LockManager#unlock(int)
 	 * @see suncertify.db.DBMain#unlock(int)
 	 */
 	@Override
-	public void unlock(final int recNo) throws RecordNotFoundException {
+	public final void unlock(final int recNo) throws RecordNotFoundException {
 		lockManager.unlock(recNo);
 	}
 
 	
-	/* (non-Javadoc)
-	 * This method calls on the update implementation located at
+	/**
+	 * This method calls on the isLocked(int) implementation 
+	 * located at  {@link suncertify.db.LockManager#isLocked(int)}.
+	 * 
+
+	 * @param recNo : record number to be locked
+	 * @throws RecordNotFoundException if new record does'nt exists
 	 * @see suncertify.db.LockManager#isLocked(int)
 	 * @see suncertify.db.DBMain#isLocked(int)
+	 * @return boolean : if the record is already locked return true
 	 */
 	@Override
-	public boolean isLocked(final int recNo) throws RecordNotFoundException {
+	public final boolean isLocked(final int recNo) 
+			throws RecordNotFoundException {
 		return lockManager.isLocked(recNo);
 	}
 }
